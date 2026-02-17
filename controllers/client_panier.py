9bb2de934af37718945bcd2e76b9717ceb67fac8
Idrissb07 <noreply@github.com>
2026-02-17 20:51:40 +0100

    travaille fait

--- controllers/client_panier.py
+++ controllers/client_panier.py
@@ -1,147 +1,202 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 from flask import Blueprint
-from flask import request, render_template, redirect, abort, flash, session
+from flask import request, render_template, redirect, session
 
 from connexion_db import get_db
 
 client_panier = Blueprint('client_panier', __name__,
-                        template_folder='templates')
+                          template_folder='templates')
+
+
+@client_panier.route('/client/panier/show')
+def client_panier_show():
+    mycursor = get_db().cursor()
+    id_client = session['id_user']
+
+    sql = '''
+    SELECT lp.ski_id AS id_ski,
+           s.nom_ski AS nom,
+           s.prix_ski AS prix,
+           s.stock,
+           lp.quantite_panier AS quantite
+    FROM ligne_panier lp
+    JOIN ski s ON s.id_ski = lp.ski_id
+    WHERE lp.utilisateur_id = %s
+    '''
+    mycursor.execute(sql, (id_client,))
+    skis_panier = mycursor.fetchall()
+
+    prix_total = 0
+    for item in skis_panier:
+        prix_total += item['prix'] * item['quantite']
+
+    return render_template('client/boutique/panier.html',
+                           skis_panier=skis_panier,
+                           prix_total=prix_total)
 
 
 @client_panier.route('/client/panier/add', methods=['POST'])
 def client_panier_add():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    id_ski = request.form.get('id_ski')
-    quantite = request.form.get('quantite')
-    # ---------
-    #id_declinaison_ski=request.form.get('id_declinaison_ski',None)
-    id_declinaison_ski = 1
+    id_ski = request.form.get('id_ski', type=int)
+    quantite = request.form.get('quantite', type=int)
 
-# ajout dans le panier d'une déclinaison d'un ski (si 1 declinaison : immédiat sinon => vu pour faire un choix
-    # sql = '''    '''
-    # mycursor.execute(sql, (id_ski))
-    # declinaisons = mycursor.fetchall()
-    # if len(declinaisons) == 1:
-    #     id_declinaison_ski = declinaisons[0]['id_declinaison_ski']
-    # elif len(declinaisons) == 0:
-    #     abort("pb nb de declinaison")
-    # else:
-    #     sql = '''   '''
-    #     mycursor.execute(sql, (id_ski))
-    #     ski = mycursor.fetchone()
-    #     return render_template('client/boutique/declinaison_ski.html'
-    #                                , declinaisons=declinaisons
-    #                                , quantite=quantite
-    #                                , ski=ski)
+    sql = '''SELECT quantite_panier 
+             FROM ligne_panier 
+             WHERE utilisateur_id = %s AND ski_id = %s'''
+    mycursor.execute(sql, (id_client, id_ski))
+    ligne = mycursor.fetchone()
 
-# ajout dans le panier d'un ski
+    if ligne is not None:
+        sql = '''UPDATE ligne_panier 
+                 SET quantite_panier = quantite_panier + %s 
+                 WHERE utilisateur_id = %s AND ski_id = %s'''
+        mycursor.execute(sql, (quantite, id_client, id_ski))
+    else:
+        sql = '''INSERT INTO ligne_panier(utilisateur_id, ski_id, quantite_panier)
+                 VALUES (%s, %s, %s)'''
+        mycursor.execute(sql, (id_client, id_ski, quantite))
 
+    #sql = '''UPDATE ski SET stock = stock - %s WHERE id_ski = %s'''
+    #mycursor.execute(sql, (quantite, id_ski))
 
-    return redirect('/client/ski/show')
+    get_db().commit()
+    return redirect('/client/panier/show')
+
 
 @client_panier.route('/client/panier/delete', methods=['POST'])
 def client_panier_delete():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    id_ski = request.form.get('id_ski','')
-    quantite = request.form.get(quantite)
+    id_ski = request.form.get('id_ski', type=int)
 
-    # ---------
-    # partie 2 : on supprime une déclinaison de l'ski
-    # id_declinaison_ski = request.form.get('id_declinaison_ski', None)
+    sql = '''SELECT quantite_panier 
+             FROM ligne_panier 
+             WHERE utilisateur_id = %s AND ski_id = %s'''
+    mycursor.execute(sql, (id_client, id_ski))
+    ligne = mycursor.fetchone()
 
-    sql = ''' SELECT * FROM ligne_panier WHERE ski_id = %s AND utilisateur_id=%s'''
-    mycursor.execute(sql, (id_ski, id_client))
-    ski_panier=mycursor.fetchone()
+    if ligne is not None:
+        if ligne['quantite_panier'] > 1:
+            sql = '''UPDATE ligne_panier 
+                     SET quantite_panier = quantite_panier - 1 
+                     WHERE utilisateur_id = %s AND ski_id = %s'''
+            mycursor.execute(sql, (id_client, id_ski))
+        else:
+            sql = '''DELETE FROM ligne_panier 
+                     WHERE utilisateur_id = %s AND ski_id = %s'''
+            mycursor.execute(sql, (id_client, id_ski))
 
-    if not(ski_panier is None) and ski_panier['quantite'] >= 1:
-        tuple_update = (quantite, id_client, id_ski)
-        sql = ''' UPDATE ligne_panier SET quantite+%s WHERE utilisateur_id =%s AND ski_id=%s'''
-        mycursor.execute(sql, tuple_update)
-    else:
-        tuple_insert = (id_client, id_ski, quantite)
-        sql = ''' INSERT INTO ligne_panier(utilisateur_id, ski_id,quantite,date_ajout) VALUES (%s,%s,%s, current_timestamp)'''
-        mycursor.execute(sql, tuple_insert)
+        #sql = '''UPDATE ski SET stock = stock + 1 WHERE id_ski = %s'''
+        #mycursor.execute(sql, (id_ski,))
 
-    # mise à jour du stock de l'ski disponible
     get_db().commit()
-    return redirect('/client/ski/show')
-
-
-
+    return redirect('/client/panier/show')
 
 
 @client_panier.route('/client/panier/vider', methods=['POST'])
 def client_panier_vider():
     mycursor = get_db().cursor()
-    client_id = session['id_user']
-    sql = ''' sélection des lignes de panier'''
-    items_panier = []
-    for item in items_panier:
-        sql = ''' suppression de la ligne de panier de l'ski pour l'utilisateur connecté'''
+    id_client = session['id_user']
 
-        sql2=''' mise à jour du stock de l'ski : stock = stock + qté de la ligne pour l'ski'''
-        get_db().commit()
-    return redirect('/client/ski/show')
+    sql = '''SELECT ski_id, quantite_panier 
+             FROM ligne_panier 
+             WHERE utilisateur_id = %s'''
+    mycursor.execute(sql, (id_client,))
+    lignes = mycursor.fetchall()
+
+    #for ligne in lignes:
+     #   sql = '''UPDATE ski
+      #           SET stock = stock + %s
+       #          WHERE id_ski = %s'''
+        #mycursor.execute(sql, (ligne['quantite_panier'], ligne['ski_id']))
+
+    sql = '''DELETE FROM ligne_panier WHERE utilisateur_id = %s'''
+    mycursor.execute(sql, (id_client,))
+
+    get_db().commit()
+    return redirect('/client/panier/show')
 
 
 @client_panier.route('/client/panier/delete/line', methods=['POST'])
 def client_panier_delete_line():
     mycursor = get_db().cursor()
     id_client = session['id_user']
-    #id_declinaison_ski = request.form.get('id_declinaison_ski')
+    id_ski = request.form.get('id_ski', type=int)
 
-    sql = ''' selection de ligne du panier '''
+    sql = '''SELECT quantite_panier 
+             FROM ligne_panier 
+             WHERE utilisateur_id = %s AND ski_id = %s'''
+    mycursor.execute(sql, (id_client, id_ski))
+    ligne = mycursor.fetchone()
 
-    sql = ''' suppression de la ligne du panier '''
-    sql2=''' mise à jour du stock de l'ski : stock = stock + qté de la ligne pour l'ski'''
+    if ligne is not None:
+        quantite = ligne['quantite_panier']
+
+        sql = '''DELETE FROM ligne_panier 
+                 WHERE utilisateur_id = %s AND ski_id = %s'''
+        mycursor.execute(sql, (id_client, id_ski))
+
+        #sql = '''UPDATE ski
+         #        SET stock = stock + %s
+          #       WHERE id_ski = %s'''
+        #mycursor.execute(sql, (quantite, id_ski))
 
     get_db().commit()
-    return redirect('/client/ski/show')
+    return redirect('/client/panier/show')
 
 
-@client_panier.route('/client/panier/filtre', methods=['POST'])
-def client_panier_filtre():
-    filter_word = request.form.get('filter_word', None)
-    filter_prix_min = request.form.get('filter_prix_min', None)
-    filter_prix_max = request.form.get('filter_prix_max', None)
-    filter_types = request.form.getlist('filter_types', None)
+@client_panier.route('/client/panier/valider', methods=['POST'])
+def client_panier_valider():
+    mycursor = get_db().cursor()
+    id_client = session['id_user']
 
-    if filter_word or filter_word == "":
-        if len(filter_word) > 1:
-            filter_word = filter_word.strip()
-            if len(filter_word) > 1:
-                session['filter_word'] = filter_word
-            else:
-                session.pop('filter_word', None)
-        else:
-            session.pop('filter_word', None)
+    # 1. Créer la commande
+    sql = '''
+        INSERT INTO commande(utilisateur_id, etat_id)
+        VALUES (%s, 1)
+    '''
+    mycursor.execute(sql, (id_client,))
+    id_commande = mycursor.lastrowid
 
-    if filter_prix_min or filter_prix_max:
-        if filter_prix_min.isnumeric():
-            session['filter_prix_min'] = filter_prix_min
-        else:
-            session.pop('filter_prix_min', None)
-        if filter_prix_max.isnumeric():
-            session['filter_prix_max'] = filter_prix_max
-        else:
-            session.pop('filter_prix_max', None)
-    
-    if filter_types:
-        session['filter_types'] = filter_types
-    else:
-        session['filter_types'] = []
-    
-    return redirect('/client/ski/show')
+    # 2. Récupérer les lignes du panier
+    sql = '''
+        SELECT * FROM ligne_panier
+        WHERE utilisateur_id = %s
+    '''
+    mycursor.execute(sql, (id_client,))
+    lignes = mycursor.fetchall()
 
+    # 3. Copier dans ligne_commande et réduire le stock
+    for ligne in lignes:
+        sql = '''
+            INSERT INTO ligne_commande(commande_id, ski_id, quantite)
+            VALUES (%s, %s, %s)
+        '''
+        mycursor.execute(sql, (
+            id_commande,
+            ligne['ski_id'],
+            ligne['quantite_panier']
+        ))
 
-@client_panier.route('/client/panier/filtre/suppr', methods=['POST'])
-def client_panier_filtre_suppr():
-    session.pop('filter_word', None)
-    session.pop('filter_types', None)
-    session.pop('filter_prix_min', None)
-    session.pop('filter_prix_max', None)
-    print("suppr filtre")
-    return redirect('/client/ski/show')
+        sql = '''
+            UPDATE ski
+            SET stock = stock - %s
+            WHERE id_ski = %s
+        '''
+        mycursor.execute(sql, (
+            ligne['quantite_panier'],
+            ligne['ski_id']
+        ))
+
+    # 4. Vider le panier
+    sql = '''
+        DELETE FROM ligne_panier
+        WHERE utilisateur_id = %s
+    '''
+    mycursor.execute(sql, (id_client,))
+
+    get_db().commit()
+    return redirect('/client/panier/show')
